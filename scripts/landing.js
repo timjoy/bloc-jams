3364777d8cadcc8d7cda18ebb0a18b26879c3211
Timothy Joy <tim.joy47@gmail.com>
2017-05-30 12:03:50 -0500

    Resubmitting checkpoint-8-DOM with landing.js for loop alteration

---
+++ scripts/landing.js
@@ -0,0 +1,14 @@
+var animatePoints = function() {
+
+     var points = document.getElementsByClassName('point');
+     var = i
+     var = function revealPoint(i){
+        for (var i = 0; i < points.length; i ++){
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+     }
+     revealPoint();
+};
