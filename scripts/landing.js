a33d4b4e6bb0db5916a37ec1442edbe4a71ebab4
Timothy Joy <tim.joy47@gmail.com>
2017-05-31 08:34:35 -0500

    Need clarification re adding a forEach loop in animatePoints

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,29 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-     var points = document.getElementsByClassName('point');
-     var = i
-     var = function revealPoint(i){
-        for (var i = 0; i < points.length; i ++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function(points) {
+
+     var revealPoint = function (index){
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+          }
+        for (var index = 0; index < points.length; index ++){
+            revealPoint(index);
         }
-     }
-     revealPoint();
 };
+window.onload = function() {
+
+      if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+      }
+
+      var sellingPoints = document.getElementsByClassName('selling-points')[0];
+      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+      window.addEventListener('scroll', function(event) {
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+              animatePoints(pointsArray);
+      }
+  });
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,7 @@
+function forEach([points]) {
+    for (var index = 0; index < array.points; index ++;) {
+        function pCallback() {
+          console.log("point!");
+        }
+    }
+}
