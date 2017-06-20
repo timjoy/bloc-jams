54135907d827641787c0c7354f679e05cbd4d87f
Timothy Joy <tim.joy47@gmail.com>
2017-06-20 17:15:57 -0500

    Added jQuery file and language

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
 
-var animatePoints = function(points) {
+    var revealPoint = function() {
 
-    var revealPoint = function (index){
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+      $(this).css({
+           opacity: 1,
+           transform: 'scaleX(1) translateY(0)'
+       });
+    };
 
-    for (var index = 0; index < points.length; index++){
-      revealPoint(index);
-    }
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
+$(window).load(function() {
 
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+  if ($(window).height() > 950) {
+         animatePoints();
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
     }
   });
-}
+});
